f2318c562ee06cbb4bb34ecff353765a9f43f895
m.masarnovsky <m.masarnovsky@gmail.com>
2020-10-28 14:12:45 +0300

    redundant methods were removed | small fixes

--- src/main/java/by/masarnovsky/Main.kt
+++ src/main/java/by/masarnovsky/Main.kt
@@ -6,8 +6,10 @@ import com.elbekD.bot.types.InlineKeyboardMarkup
 import com.elbekD.bot.types.Message
 import com.mongodb.MongoClient
 import com.mongodb.MongoClientURI
+import com.mongodb.client.FindIterable
 import com.mongodb.client.MongoDatabase
 import com.mongodb.client.model.Filters.eq
+import com.mongodb.client.model.Filters.gt
 import org.bson.Document
 import java.io.FileInputStream
 import java.util.*
@@ -29,7 +31,7 @@ fun loadProperties() {
     myId = properties.getProperty("myid")
 }
 
-fun main(args: Array<String>) {
+fun main() {
     loadProperties()
     val bot = Bot.createPolling(username, token)
 
@@ -51,18 +53,9 @@ fun main(args: Array<String>) {
         val data = callback.data!!
         val chatId = callback.message?.chat?.id!!
 
-        println(data)
-        if (data.startsWith("debtor_")) {
-            val name = data.replace("debtor_", "")
-            println(name)
-            // TODO ("Подумать, нужен ли вообще этот метод")
-        } else {
-            when (data) {
-                "callback_add" -> addDebtNote(chatId, bot)
-                "callback_new" -> addNewDebtorMessage(chatId, bot)
-                "callback_exists" -> getListOfDebtors(chatId, bot)
-                else -> returnListOfDebtorsForChat(chatId, bot)
-            }
+        when (data) {
+            "callback_list" -> returnListOfDebtorsForChat(chatId, bot)
+            else -> returnListOfDebtorsForChat(chatId, bot)
         }
     }
 
@@ -86,20 +79,21 @@ fun repay(bot: Bot, message: Message) {
     val debtor = updateDebtor(name, sum, "Возврат суммы", message.chat.id)!!
     bot.sendMessage(
         message.chat.id,
-        "Теперь ${debtor["name"]} должен тебе ${debtor["sum"]} BYN за ${debtor["comments"]}"
+        "${debtor["name"]} вернул(а) $sum BYN и теперь торчит ${debtor["sum"]} BYN"
     )
 }
 
 fun updateDebtor(name: String, sum: String, comment: String, chatId: Long): Document? {
+    val lowercaseName = name.toLowerCase()
     val connectionString = MongoClientURI("mongodb://localhost:27017")
     val mongoClient = MongoClient(connectionString)
     val database: MongoDatabase = mongoClient.getDatabase("debot")
     val collection = database.getCollection("debts")
-    var debtor = collection.find(Document("name", name).append("chatId", chatId)).first()
+    var debtor = collection.find(Document("name", lowercaseName).append("chatId", chatId)).first()
     var newSum = sum.toDouble()
 
     if (debtor == null) {
-        debtor = Document("name", name).append("sum", newSum).append("comments", mutableListOf(comment))
+        debtor = Document("name", lowercaseName).append("sum", newSum).append("comments", mutableListOf(comment))
             .append("chatId", chatId)
         collection.insertOne(debtor)
     } else {
@@ -107,7 +101,7 @@ fun updateDebtor(name: String, sum: String, comment: String, chatId: Long): Docu
         debtor["sum"] = newSum
         val comments: MutableList<String> = debtor["comments"] as MutableList<String>
         comments.add(comment)
-        collection.updateOne(eq("name", name), Document("\$set", debtor))
+        collection.updateOne(eq("name", lowercaseName), Document("\$set", debtor))
     }
 
     mongoClient.close()
@@ -115,45 +109,28 @@ fun updateDebtor(name: String, sum: String, comment: String, chatId: Long): Docu
 }
 
 private fun mainMenu(bot: Bot, msg: Message) {
-    val add = InlineKeyboardButton(text = "Добавить", callback_data = "callback_add")
     val list = InlineKeyboardButton(text = "Список всех", callback_data = "callback_list")
-    val keyboard = InlineKeyboardMarkup(listOf(listOf(add, list)))
+    val keyboard = InlineKeyboardMarkup(listOf(listOf(list)))
     bot.sendMessage(
         msg.chat.id,
-        "Ты можешь добавить человека в список, либо посмотреть всех, кто тебе должен",
+        "Добавляй должника в таком формате: {имя} {сумма} {комментарий}, либо {имя} -{сумма} чтобы вычесть сумму долга. Кнопочка чтобы посмотреть всех",
         markup = keyboard
     )
 }
 
 fun returnListOfDebtorsForChat(chatId: Long, bot: Bot) {
     var result = ""
-    getNames().forEach { (key, value) -> result += "$key $value BYN\n" }
-    bot.sendMessage(chatId, result)
+    val names = getDebtors()
+    names.forEach { document -> result += "${document["name"]} ${document["sum"]} BYN\n" }
+    bot.sendMessage(chatId, if (result.isNotEmpty()) result else "Пока что никто тебе не должен")
 }
 
-fun addDebtNote(chatId: Long, bot: Bot) {
-    val new = InlineKeyboardButton(text = "Новый", callback_data = "callback_new")
-    val exists = InlineKeyboardButton(text = "Выбрать из существующих", callback_data = "callback_exists")
-    val keyboard = InlineKeyboardMarkup(listOf(listOf(new, exists)))
-    bot.sendMessage(chatId, "Добавь нового должника или выбери уже существующего", markup = keyboard)
-}
-
-fun addNewDebtorMessage(chatId: Long, bot: Bot) {
-    bot.sendMessage(chatId, "Отправь имя, сумму и комментарий к долгу в формате {имя} {сумма} {комментарий}")
-}
-
-fun getListOfDebtors(chatId: Long, bot: Bot) {
-    val names = mutableListOf<InlineKeyboardButton>()
-    getNames().forEach { (key, _) ->
-        names.add(InlineKeyboardButton(text = key, callback_data = "debtor_$key"))
-    }
-    val keyboard = InlineKeyboardMarkup(listOf(names))
-    bot.sendMessage(chatId, "Выбери должника", markup = keyboard)
-}
-
-
-fun getNames(): Map<String, Double> {
-    return mutableMapOf("stas" to 14.08, "max" to 6.66, "nina" to 14.88)
+fun getDebtors(): FindIterable<Document> {
+    val connectionString = MongoClientURI("mongodb://localhost:27017")
+    val mongoClient = MongoClient(connectionString)
+    val database: MongoDatabase = mongoClient.getDatabase("debot")
+    val collection = database.getCollection("debts")
+    return collection.find(gt("sum", 0))
 }
 
 fun isStringMatchDebtPattern(str: String): Boolean {
